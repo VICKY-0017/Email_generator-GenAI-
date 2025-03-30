b6b3b3ca2b03430e3fda5d735179cd2062d07e41
VIGNESH BABU T R <110011481+VICKY-0017@users.noreply.github.com>
2025-03-30 19:07:23 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from langchain_community.document_loaders import WebBaseLoader
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
+import pysqlite3
+import sys
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 
 def create_streamlit_app(llm, portfolio, clean_text):
